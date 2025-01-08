69cf6a9226928d97433d3378729d1f686af09c2b
Anurag <Negianurag99@gmail.com>
2025-01-08 13:05:21 +0530

    added crerate line function

--- Window.py
+++ Window.py
@@ -23,7 +23,24 @@ class window:
             self.redraw()
         print("window closed...")
 
+    def draw_line(self, line, fillcolour):
+        line.draw(self.canvas, fillcolour)
+
     def close(self):
         self.window_running = False 
 
+
+class point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+
+class line:
+    def __init__(self, p1, p2) -> None:
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fillcolour):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y ,fill= fillcolour, width=2)
 
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 
-from Window import window 
+from Window import window, line, point
  
 def main():
     win= window(800, 600)
+    create_line = line(point(30, 40), point(50, 80))
+    create_line2= line(point(90, 70), point(85, 10))
+    create_line3= line(point(60, 100), point(50, 75))
+    create_line.draw(win.canvas, fillcolour="red")
+    create_line2.draw(win.canvas, fillcolour="red")
+    create_line3.draw(win.canvas, fillcolour="red")
     win.wait_for_close()
+    
 
 if __name__ == "__main__":
     main()
