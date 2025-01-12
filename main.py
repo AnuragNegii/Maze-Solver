0afd22c106302485f8dba41ca2db923c456014fa
Anurag <Negianurag99@gmail.com>
2025-01-12 14:41:23 +0530

    created maze solver

--- main.py
+++ main.py
@@ -12,8 +12,13 @@ def main():
     cell_size_y = (screen_y - 2 * margin) / num_rows
     win = window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win ,10)
-    win.wait_for_close()
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win ,1)
+    is_solvable = maze.solve()
+    if not is_solvable:
+        print("maze can not be solved!")
+    else:
+        print("maze solved!")
+    win.wait_for_close()   
 
 if __name__ == "__main__":
     main()
